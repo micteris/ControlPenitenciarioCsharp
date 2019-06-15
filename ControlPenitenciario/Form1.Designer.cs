ecfe84fc714d00e53b036ff6cc2bf4d6bc2d8ef7
micteris <jose.clarosh@yahoo.es>
2019-06-15 13:44:11 -0600

    arreglos menores

--- ControlPenitenciario/Form1.Designer.cs
+++ ControlPenitenciario/Form1.Designer.cs
@@ -41,11 +41,11 @@
             this.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSalir.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnSalir.Location = new System.Drawing.Point(480, 12);
+            this.btnSalir.Location = new System.Drawing.Point(507, 12);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(48, 28);
             this.btnSalir.TabIndex = 1;
             this.btnSalir.Text = "X";
             this.btnSalir.UseVisualStyleBackColor = false;
